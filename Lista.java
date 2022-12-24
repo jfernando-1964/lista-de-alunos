7227ae925e6ee97fa4a1093c05f061d93787eb0d
jfernando-1964 <fariasilva@bol.com.br>
2022-12-24 02:11:50 -0300

    first commit

---
+++ Lista.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Lista {
+    public static void main(String[] args){
+        Scanner sx = new Scanner(System.in);
+
+        String dia[] = {"01/01/2021", "02/01/2021", "03/01/2021"};
+        String alunos[] = {"fulano", "clicano", "deltano"};
+
+        String chamada[] = new String[dia.length][alunos.length];
+
+        sx.close();
+
+        for(int i = 0; i < dia.length; i++){
+            System.out.println("Chamada do dia " + dia[i]);
+            System.out.println("Aluno " + alunos[i] + " está presente?(sim/não)");
+            String yn = sx.next();
+            if(yn.equals("sim")){
+                System.out.println("Ok");
+
+            }else{
+                System.out.println("Ok, falta marcada");
+            }
+        }
+    }
+}
